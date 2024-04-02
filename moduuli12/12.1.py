4d926e3395bd9d03b2c3fc62e69aeeddd2744a27
tatuvesa <tatuve@metropolia.fi>
2024-04-02 08:11:56 +0200

    le pushe

--- moduuli12/12.1.py
+++ moduuli12/12.1.py
@@ -1,10 +1,10 @@
 import requests
 
 
-def chuck():
+def monke():
     url = "https://api.chucknorris.io/jokes/random"
     data = requests.get(url).json()
     print(data["value"])
 
 
-chuck()
+monke()
