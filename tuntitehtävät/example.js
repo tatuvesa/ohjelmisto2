9faeceb84b318ee2e11c29b9e631071719b03ab5
tatuvesa <tatuve@metropolia.fi>
2024-04-05 10:15:28 +0200

    le pushe

---
+++ tuntitehtävät/example.js
@@ -0,0 +1,7 @@
+'use strict';
+const name = 'Frank';
+document.querySelector('#target').innerHTML = 'Good morning, ' + name + '!';
+console.log(typeof name);
+    const age = 23;
+console.log(typeof age)
+    const ageStr = age.toString();
